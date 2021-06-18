37bf0e7d4ebbb9e2fdea39a6014e7b1fcc25c067
Jason Fors <78061274+jason-fors@users.noreply.github.com>
2021-06-18 14:30:07 -0400

    Update Aggregate_Files.py

--- Aggregate_Files.py
+++ Aggregate_Files.py
@@ -3,18 +3,28 @@ import os
 
 
 
-
 def unzipAndAppend(year, date, ticker, thisFile):
     nextFile = "C:\\Users\\jason\\OneDrive\\Documents\\Old PC\\Model_Test\\fut_trades_1min\\" + year + "\\" + date + "\\" + ticker + "\\" + thisFile
-    with gzip.open(nextFile, 'rt') as unzippedFile:     # Opens a .gz file
+    with gzip.open(nextFile, 'rb') as unzippedFile:     # Opens a .gz file
         file_content = unzippedFile.read()
     with open('C:\\Users\\jason\\OneDrive\\Documents\\Old PC\\Model_Test\\AllRecords.csv', "a") as f:  # Appending records from unzipped file to aggregate file. 
         f.write(file_content)
         file_content.close()  # Close file to prevent memory leak
 
+def getFileLength(file):
+    print("Called getFileLenth method, which has not been defined yet.")
+
 
 if __name__ == "__main__":
+    folderPath = "C:\\Users\\Jason Fors\\Documents\\GitHub\\Python-Projects"
+    folderDirectory = os.listdir(path=folderPath)
     
+    for i in folderDirectory:
+        longest = 0
+        
+        if ".txt" in i:
+            fullPath = os.path.join(folderPath, i)
+            print(fullPath + "\n Seconds since the Epoch: " + str(os.path.getmtime(fullPath)))
 
 
 
