173298f1c6d781646b52bf115a169cb12d3a6228
Jason Fors <78061274+jason-fors@users.noreply.github.com>
2021-06-27 13:57:24 -0400

    Update Aggregate_Files.py
    Finalize file-length function

--- Aggregate_Files.py
+++ Aggregate_Files.py
@@ -30,25 +30,28 @@ def appendData(iFilePath):
         file_content = unzippedFile.read()
         print(len(file_content))
         unzippedFile.close()
-    with open('C:\\Users\\jason\\OneDrive\\Documents\\Old PC\\Model_Test\\AllRecords.csv', "w") as f:  # Writing records from unzipped file to aggregate file. 
+    with open('C:\\Users\\jason\\OneDrive\\Documents\\Old PC\\Model_Test\\AllRecords.csv', "a") as f:  # Writing records from unzipped file to aggregate file. 
         f.write(file_content[1:])       # Need to skip the first line so we don't get a new header line for every file.
         f.close()  
 
 
-# Function for finding longest file in a folder
+# Function for finding longest file in a folder.  This will be treated as the active contract for trading on that day.
 def findActiveContract(tickerPath):        #  Pass in the current directory
     tickerDirectory = os.listdir(path=tickerPath)
+    fileLength = 0
     for i in tickerDirectory:
-        fileLength = 0     # Set initial value to 0. Need to find the file with the most records in each folder -- this will be the most-traded contract.
         if "gz" in i:       # Don't want to include unzipped or any other file types.
-            iFilePath = tickerPath + i
+            iFilePath = tickerPath + "//" + i
             with gzip.open(iFilePath, 'rt') as nextFile:
                 file_content = nextFile.read()
+                print(iFilePath)
+                print("Length of next file: ")
+                print(len(file_content))
                 if len(file_content) > fileLength:
                     fileLength = len(file_content)
                     longestFilePath = iFilePath
+                    print(fileLength)
                     print(longestFilePath) 
-                    print(iFilePath)
                 nextFile.close()
     return longestFilePath
 
@@ -62,12 +65,14 @@ if __name__ == "__main__":
         yearDirectory = os.listdir(path=yearPath)
         print(yearDirectory)
         for day in yearDirectory:
-            dayPath = yearDirectory + "\\" + day
+            dayPath = yearPath + "\\" + day
             dayDirectory = os.listdir(path=dayPath)
             print(dayDirectory)
             for ticker in dayDirectory:
                 tickerPath = dayPath + "\\" + ticker
                 tickerDirectory = os.listdir(path=tickerPath)
                 print(tickerDirectory)
-                # fileToAppend = findActiveContract(tickerPath)
+                fileToAppend = findActiveContract(tickerPath)
+                print(fileToAppend + " ------ File to append")
                 # appendData(fileToAppend)
+            print("End of day.")
