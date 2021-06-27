dec6e8d69da71ad16eb232466e88e48783c12427
Jason Fors <78061274+jason-fors@users.noreply.github.com>
2021-06-27 16:28:56 -0400

    Finish Aggregate_Files
    Finish aggregate_files program and update ReadMe.

--- Aggregate_Files.py
+++ Aggregate_Files.py
@@ -14,25 +14,20 @@ import gzip
 import os
 
 
-# # Doing this a different way.
-# def unzipAndAppend(year, date, ticker, thisFile):
-#     nextFile = "C:\\Users\\jason\\OneDrive\\Documents\\Old PC\\Model_Test\\fut_trades_1min\\" + year + "\\" + date + "\\" + ticker + "\\" + thisFile
-#     with gzip.open(nextFile, 'rb') as unzippedFile:     # Opens a .gz file
-#         file_content = unzippedFile.read()
-#     with open('C:\\Users\\jason\\OneDrive\\Documents\\Old PC\\Model_Test\\AllRecords.csv', "a") as f:  # Appending records from unzipped file to aggregate file. 
-#         f.write(file_content)
-#         file_content.close()  # Close file to prevent memory leak
-
-
 # Function for appending content from a selected file to the aggregate file
 def appendData(iFilePath):
     with gzip.open(iFilePath, 'rt') as unzippedFile:
-        file_content = unzippedFile.read()
+        file_content = unzippedFile.readlines()
         print(len(file_content))
         unzippedFile.close()
-    with open('C:\\Users\\jason\\OneDrive\\Documents\\Old PC\\Model_Test\\AllRecords.csv', "a") as f:  # Writing records from unzipped file to aggregate file. 
-        f.write(file_content[1:])       # Need to skip the first line so we don't get a new header line for every file.
-        f.close()  
+    with open('C:\\Users\\jason\\OneDrive\\Documents\\Old PC\\Model_Test\\AllRecords.csv', "a+") as f:  # Writing records from unzipped file to aggregate file. a+ means append and create if it doesn't exist.
+        count = 0
+        for line in file_content:
+            print(line)
+            if count != 0:          # Need to skip the first line so we don't get a new header line for every file.
+                f.write(line)
+            count += 1
+        f.close()
 
 
 # Function for finding longest file in a folder.  This will be treated as the active contract for trading on that day.
@@ -43,7 +38,7 @@ def findActiveContract(tickerPath):        #  Pass in the current directory
         if "gz" in i:       # Don't want to include unzipped or any other file types.
             iFilePath = tickerPath + "//" + i
             with gzip.open(iFilePath, 'rt') as nextFile:
-                file_content = nextFile.read()
+                file_content = nextFile.readlines()
                 print(iFilePath)
                 print("Length of next file: ")
                 print(len(file_content))
@@ -74,5 +69,6 @@ if __name__ == "__main__":
                 print(tickerDirectory)
                 fileToAppend = findActiveContract(tickerPath)
                 print(fileToAppend + " ------ File to append")
-                # appendData(fileToAppend)
+                appendData(fileToAppend)
             print("End of day.")
+            print("Should have stopped.")
--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Backtesting-App
+
+
+### Aggregate_Files
+Goes through all the files purchased from vendor and aggregates the desired case data into one file. 
+This required:
+1. Trarversing the (2,273) files in the folder structure (3 layers deep, 1,193 folders) and:
+    a. Cycling through for each ticker (ES or YM) within each day,
+    b. Identifying which contract makes the most sense for that day (the file with the most records should work)
+    c. Add those records into an aggregate file.
+2. Repeat this process for each ticker, putting them all in one file. 
+
+The data files were compressed with .gz.  Needed to use gzip library in Python.
+
+### Backtesting Tool
+Will allow for testing pairs trading algorithms on the year of by-minute ticker data.
+
