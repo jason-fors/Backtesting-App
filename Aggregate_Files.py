6153b6ea3802b0b892b427a3a227b98bcea9aca8
Jason Fors <78061274+jason-fors@users.noreply.github.com>
2021-06-27 16:55:41 -0400

    Add backtesting_tool
    Add starting point for backtesting tool.

--- Aggregate_Files.py
+++ Aggregate_Files.py
@@ -1,12 +1,12 @@
-""" Go through all the files purchased from vendor and aggregate the desired case data into one file. 
-This will require:
-1. Moving through all the (2,273) files in the folder structure (3 layers deep, 1,193 folders) and:
-    a. Identifying the correct folders for each ticker (ES or YM) within each day,
+""" Goes through all the files purchased from vendor and aggregates the desired case data into one file. 
+This required:
+1. Trarversing the (2,273) files in the folder structure (3 layers deep, 1,193 folders) and:
+    a. Cycling through for each ticker (ES or YM) within each day,
     b. Identifying which contract makes the most sense for that day (the file with the most records should work)
     c. Add those records into an aggregate file.
 2. Repeat this process for each ticker, putting them all in one file. 
 
-.gz files are compressed.  Need to unzip as well.
+The data files were compressed with .gz.  Needed to use gzip library in Python.
 
 """
 
@@ -24,7 +24,7 @@ def appendData(iFilePath):
         count = 0
         for line in file_content:
             print(line)
-            if count != 0:          # Need to skip the first line so we don't get a new header line for every file.
+            if count != 0:          # Need to skip the first line so we don't get the header row for every file.
                 f.write(line)
             count += 1
         f.close()
@@ -70,5 +70,4 @@ if __name__ == "__main__":
                 fileToAppend = findActiveContract(tickerPath)
                 print(fileToAppend + " ------ File to append")
                 appendData(fileToAppend)
-            print("End of day.")
-            print("Should have stopped.")
+
